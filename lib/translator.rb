bc1800427537ec93bc56a3c117f2cc213581ea53
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 15:59:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,13 @@ def load_library(file)
   full_file = YAML.load_file(file)
   emoticons = {}
   emoticons["get_emoticon"] = {}
+  file.each do |meaning, emote|
+    emoticons["get_emoticon"][emote[1]] = meaining
+  end
   emoticons["get_meaning"] = {}
-  
+  file.each do |meaning, emote|
+    emoticons["get_meaning"][]
+  end
 end
 
 def get_japanese_emoticon
