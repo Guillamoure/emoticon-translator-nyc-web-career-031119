f3d4d4e0dc23d366323817852669af034975d868
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 16:06:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(yaml, emo)
   # code goes here
   emoticons = load_library(yaml)
-  binding.pry
+  
   emoticons["get_emoticon"].each do |english, japanese|
     if english == emo
       return japanese
